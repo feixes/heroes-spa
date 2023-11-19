2d6fe42e469e074e7146076e798f8c2c1ea951db
Xavier Feixas <xavierfeixesguell@gmail.com>
2023-11-19 21:59:53 +0100

    animations

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -62,11 +62,11 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr />
 
-                    <div className="alert alert-primary" style={{ display: showSearch ? '' : 'none' }}>
+                    <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>
                         Search a hero
                     </div>
 
-                    <div className="alert alert-danger" style={{ display: showError ? '' : 'none' }}>
+                    <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
                         No hero with <b>{q}</b>
                     </div>
 
