cccc73683c6474e76313253db7232bd3bed84600
Xavier Feixas <xavierfeixesguell@gmail.com>
2023-11-20 11:48:32 +0100

    context, reducer, login and logout with no backend

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,14 +1,21 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 
 export const LoginPage = () => {
 
     const navigate = useNavigate()
 
+    const { login } = useContext(AuthContext)
+
     const onLogin = () => {
+        login("Xavier Feixas")
+
         navigate('/', {
             replace: true
         })
+
     }
 
     return (
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,16 +1,24 @@
 
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
+    const { user, logout } = useContext(AuthContext)
     const navigate = useNavigate()
 
     const onLogout = () => {
+
+        logout()
+
         navigate('/login', {
             replace: true
         })
     }
+
+
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark px-3">
@@ -51,7 +59,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Xavier
+                        {user?.name}
                     </span>
                     <button
                         className='nav-item nav-link btn'
